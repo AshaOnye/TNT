56568c7d6db85328dbdfe13467345ddc4d22cb77
AshaOnye <ashiefortune@gmail.com>
2023-09-25 22:45:49 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,339 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>About | TNT Experiential Novus</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon.png" />
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/gijgo.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img src="img/logo.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="index.php">home</a></li>
+                                        <li><a class="active"
+                                                href="about.php">About</a></li>
+                                        <li><a href="services.php">Services</a>
+                                        </li>
+                                        <li><a href="contact.php">Contact</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                            <div class="Appointment">
+                                <div class="book_btn d-none d-lg-block">
+                                    <a href="contact.php">Need Help?</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- bradcam_area  -->
+    <div class="slider_area">
+        <div class=" d-flex align-items-center slider_bg_1">
+            <div class="shap_pattern d-none d-lg-block">
+                <img src="img/about/grid.png" alt="">
+            </div>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-12">
+                        <div class="bradcam_text text-center">
+                            <h3></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /bradcam_area  -->
+
+    <div class="service_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title text-center">
+                        <span></span>
+                        <h6>About Us</h6>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+
+
+    <!-- about_me  -->
+    <div class="about_me">
+        <!-- <div class="about_large_title d-none d-lg-block">
+            About
+        </div> -->
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-md-6">
+                    <div class="about_e_details">
+                    <h3>TNT Experiential Novus</h3>
+                        <p>TNT Experiential Novus, a fast-rising Public
+                            Relations and Creative Agency that utilizes a
+                            diverse group of strategic and creative teams, who
+                            work tirelessly and devotedly to find the right,
+                            workable and sustainable answers to your brand's
+                            public relations and creative challenges hereby
+                            building positive relationships with the public and
+                            key stakeholders to help achieve your business
+                            goals.<br><br>
+
+                            <span class="whut">WHAT WE DO:</span><br>
+                            ● We apply a tailored and custom approach that turns
+                            your brand into our brand.<br>
+                            ● We give your brand an experiential approach
+                            through the stages of brand strategy, visual
+                            identity, and brand positioning.<br>
+                            ● We give unparalleled ideas for your brand with our
+                            finely honed and ever-evolving creative approach
+                            that creates valuable experience for your brand and
+                            target audience. <br><br>
+
+                            Essentially, TNT Experiential Novus specializes in
+                            Brand Consultation and Brand Development,
+                            Experiential Marketing, Digital Marketing, Affiliate
+                            Marketing, Photography and Videography, Professional
+                            Trainings, Production Commercialization, Crisis
+                            Communication Plans, ADS Account Fixing, Media
+                            Advertisements, and Product Branding.
+                        </p>
+
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6">
+                    <div class="about_img">
+                        <div class="color_pattern d-none d-lg-block">
+                            <img src="img/about/color_grid.png" alt="">
+                        </div>
+                        <div class="my_Pic">
+                            <img src="img/about/about.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ about_me  -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="section_title text-center mb-65">
+                    <span></span>
+                    <h3>Meet Our Team</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="portfolio-top">
+            <div class="portfolio_image_area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-md-6 col-lg-4">
+                            <div class="single_Portfolio">
+                                <div class="portfolio_thumb">
+                                    <img src="img/testmonial/66.png" alt="">
+                                </div>
+                                <a href="img/testmonal/66.png"
+                                    class="popup popup-image"></a>
+                                <div class="portfolio_hover">
+                                    <div class="title">
+                                        <h3>Tayelolu Akinbohun</h3>
+                                        <h4>Co-Founder| Communications Executive</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-6 col-lg-4">
+                            <div class="single_Portfolio">
+                                <div class="portfolio_thumb">
+                                    <img src="img/testmonial/77.png" alt="">
+                                </div>
+                                <a href="img/testmonial/77.png"
+                                    class="popup popup-image"></a>
+                                <div class="portfolio_hover">
+                                    <div class="title">
+                                        <h3>Morgan Otobong</h3>
+                                        <h4>Business Developer</h4>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-12 col-lg-4">
+                            <div class="single_Portfolio">
+                                <div class="portfolio_thumb">
+                                    <img src="img/testmonial/88.png" alt="">
+                                </div>
+                                <a href="img/testmonial/88.png"
+                                    class="popup popup-image"></a>
+                                <div class="portfolio_hover">
+                                    <div class="title">
+                                        <h3>Tolulope Omoboyowa</h3>
+                                        <h4>Co-Founder| Production Executive</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="discuss_projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="project_text text-center">
+                        <h3>Let’s discuss for a project</h3>
+                        <h4>We are available in Nigeria and Canada</h4>
+                        <a class="boxed-btn3" href="#" style="margin-top: 50px;">Start Talking</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="menu_links">
+                            <ul>
+                                <li><a href="about.php">About</a></li>
+                                <li><a href="services.php">Services</a></li>
+                                <li><a href="contact.php">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="socail_links">
+                            <ul>
+                                <li><a
+                                        href="https://www.facebook.com/profile.php?id=100077863995152&mibextid=LQQJ4d">
+                                        <i class="fa fa-facebook"></i> </a></li>
+                                <li><a
+                                        href="https://twitter.com/tntexperiential?s=21&t=LCrTAJUGKrhIf8VxY_PfgA">
+                                        <i class="fa fa-twitter"></i> </a></li>
+                                <li><a
+                                        href="https://instagram.com/tnt_experiential?igshid=YmMyMTA2M2Y=">
+                                        <i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <p class="copy_right text-center">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                            </script> All rights reserved | TNT Experiential
+                            Novus <i class="" aria-hidden="true"></i> <a
+                                href="https://colorlib.com" target="_blank">Colorlib</a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/ footer end  -->
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/gijgo.min.js"></script>
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
